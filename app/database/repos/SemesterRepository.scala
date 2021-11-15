a4e826f2812a64373af1e337dc321623f1e1d9fb
Alexander Dobrynin <info@alex-dobrynin.com>
2021-11-15 15:52:18 +0100

    current semester endpoint (#34)

--- app/database/repos/SemesterRepository.scala
+++ app/database/repos/SemesterRepository.scala
@@ -1,5 +1,6 @@
 package database.repos
 
+import database.repos.filter.BooleanParser
 import database.tables.{SemesterDbEntry, SemesterTable}
 import models.Semester
 import play.api.db.slick.{DatabaseConfigProvider, HasDatabaseConfigProvider}
@@ -13,14 +14,17 @@ class SemesterRepository @Inject() (
     val dbConfigProvider: DatabaseConfigProvider,
     implicit val ctx: ExecutionContext
 ) extends HasDatabaseConfigProvider[JdbcProfile]
-    with Repository[Semester, SemesterDbEntry, SemesterTable] {
+    with Repository[Semester, SemesterDbEntry, SemesterTable]
+    with BooleanParser {
 
   import profile.api._
 
   protected val tableQuery = TableQuery[SemesterTable]
 
-  override protected def makeFilter = { case ("label", vs) =>
-    t => t.hasLabel(vs.head)
+  override protected def makeFilter = {
+    case ("label", vs) => t => t.hasLabel(vs.head)
+    case ("current", vs) =>
+      t => parseBoolean(vs, b => if (b) t.currentFilter() else false)
   }
 
   override protected def retrieveAtom(
